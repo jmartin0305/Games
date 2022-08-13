2737652cb2823a09f788103fa77ef3fd26ddd55d
jmartin0305 <105560962+jmartin0305@users.noreply.github.com>
2022-08-13 18:58:27 -0400

    Update main.py

--- Yahtzee/main.py
+++ Yahtzee/main.py
@@ -1,1 +1,21 @@
 #test file
+
+
+
+# create current game object
+# could create a class, don't anticipate more than 2 players at a time though
+
+
+#turn
+    #first roll
+        #select type to apply to or
+        #select dice to keep and reroll remaining
+    #second roll
+        #select type to apply to or
+        #select dice to keep and reroll remaining
+    #select type to apply to
+
+# record score
+    #show what is still available
+    #prompt user selection
+    #calculate score based on selection
