b118d3ec6308d4c78b6ed76de30cb8049dabbac0
jmartin0305 <105560962+jmartin0305@users.noreply.github.com>
2022-09-13 20:07:10 -0400

    Update main.py

--- Yahtzee/main.py
+++ Yahtzee/main.py
@@ -1,9 +1,28 @@
 #test file
 
 
-
 # create current game object
 # could create a class, don't anticipate more than 2 players at a time though
+
+
+class PlayerCard:
+  def __init__(self, ones, twos, threes, fours, fives, sixes, threeKind, fourKind, fullHouse, smallStr, largeStr, yahtzee, chance):
+    self.ones = 0
+    self.twos = 0
+    self.threes = 0
+    self.fours = 0
+    self.fives = 0
+    self.sixes = 0
+    self.threeKind = 0
+    self.fourKind = 0
+    self.fullHouse = 0
+    self.smallStr = 0
+    self.largeStr = 0
+    self.yahtzee = 0
+    self.chance = 0
+
+
+
 
 
 #turn
